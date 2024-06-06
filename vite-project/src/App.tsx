ab5d4e528d65e43f9c94314cd0b3154461d1ab94
wisanu <wisanu@odds.team>
2024-06-06 19:05:57 +0700

    Deploy React wed site G1

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+function App() {
+  return (
+    <>
+      <body>
+        <div className="box">
+          <h1>Todo app</h1>
+          <input id='input' placeholder="please input here" type="text" />
+          <button id="button">add</button>
+        </div>
+        <ul>
+          <li> taks1</li>
+          <li> taks2</li>
+          <li> taks3</li>
+          <li> taks4</li>
+        </ul>
+      </body>
+    </>
+  )
+}
+
+export default App   // ส่วนนี้คือการ export คลาส App ออกไปให้โปรเจคอื่นๆ ใช้งาน
